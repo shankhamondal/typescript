169a112cf438f75f6935c9fd791b8ea2b9b5fb02
Shankha Mondal <shankhamondal.codes@gmail.com>
2026-09-25 19:22:06 +0530

    Create a New practical

---
+++ practical05.ts
@@ -0,0 +1,12 @@
+let number = 2;
+
+console.log(`${number} X 1 = ${number * 1}`);
+console.log(`${number} X 2 = ${number * 2}`);
+console.log(`${number} X 3 = ${number * 3}`);
+console.log(`${number} X 4 = ${number * 4}`);
+console.log(`${number} X 5 = ${number * 5}`);
+console.log(`${number} X 6 = ${number * 6}`);
+console.log(`${number} X 7 = ${number * 7}`);
+console.log(`${number} X 8 = ${number * 8}`);
+console.log(`${number} X 9 = ${number * 9}`);
+console.log(`${number} X 10 = ${number * 10}`);
---
+++ practical06.ts
@@ -0,0 +1,17 @@
+let discount = 10;
+let laptopPrice = 45000;
+let headphonePrice = 1500;
+let speakerPrice = 3000;
+
+console.log(`Flat Discount Rate: ${discount}%`);
+console.log(
+  `Laptop: ${laptopPrice}/- (Discount:- ${(laptopPrice * discount) / 100}/- )`,
+);
+
+console.log(
+  `HeadPhone: ${headphonePrice}/- (Discount:- ${(headphonePrice * discount) / 100}/-)`,
+);
+
+console.log(
+  `Speker Price: ${speakerPrice}/- (Discount: ${(speakerPrice * discount) / 100}/-)`,
+);
